bef7de3d4316d25c1e3545e9c0640c9ec39a076e
achuacui <a.chua@163.com>
2017-01-07 21:47:11 +0800

    update UserInfo

--- src/main/java/com/baidu/cbu/achua/module/UserInfo.java
+++ src/main/java/com/baidu/cbu/achua/module/UserInfo.java
@@ -1,18 +1,9 @@
 package com.baidu.cbu.achua.module;
 
 public class UserInfo {
-	public String userid = "56df644bb29d4ec3856ca2d0aa3497cf";
-	// achuacui0101
-	// public static String accessKey = "6148e048017246cbb0fe05b297519a30";
-	// public static String secretKey = "d9ae7b47634f4608891300437c68b053";
-
-	// achuacui0101 BCC api key
-	public String accessKey = "763232cca91c44ec8dd7f9ee5d8020f1";
-	public String secretKey = "51d91b2fe266442da4af54c45c7a1f09";
-
-	// achuacui
-	// public static String accessKey = "ZIggCKKwQfel5Kambty4PQoP";
-	// public static String secretKey = "jTc7vDlYi3FVwtu2AzLsbwXruVpFc5i9";
+	public String userid = "";
+	public String accessKey = "";
+	public String secretKey = "";
 
 	public UserInfo() {
 	}
